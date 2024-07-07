292aaf757d9c1ce867252dea2b1695bfd479ef26
Aditya Kumar <131326798+Adityasinghvats@users.noreply.github.com>
2024-07-07 12:20:39 +0000

    leant about tables

--- 01basics/hello.js
+++ 01basics/hello.js
@@ -1,5 +1,12 @@
-console.log("hello World!")
+// console.log("hello World!")
 let name = "Aditya Kumar"
-console.log(name)
+// console.log(name)
 name = 67
-console.log(name)
+// console.log(name)
+let address = "Bihat"
+let pincode = 123456
+let location =" 23.45 'N"
+console.table([name,address,pincode,location])
+
+// let table = [name,address,pincode,location]
+// console.log(table[1]) -> Biaht as output
