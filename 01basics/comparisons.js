dd44b49d7b5da3b08be5131ccb23d1ce3e8c4f37
Aditya Kumar <131326798+Adityasinghvats@users.noreply.github.com>
2024-07-07 12:01:34 +0000

    comparisons

---
+++ 01basics/comparisons.js
@@ -0,0 +1,23 @@
+// "use strict";
+// "null" is an object in js
+// console.log("02"==02) -> output is true if not used wiht "use strict" ;
+
+// console.log("2">1)-> output is true if not used wiht "use strict" ;
+// console.log("2"<1)-> output is false if not used wiht "use strict" ;
+// console.log("2" != 1)-> output is true if not used wiht "use strict" ;
+
+// console.log(undefined == 0);-> output is false
+// console.log(undefined == "0");-> output is false
+// console.log(undefined == null);-> output is true
+// console.log(null == 0);-> output is false
+
+// console.log(undefined > 0);-> output is false
+// console.log(undefined < 0);-> output is false
+// console.log(null > 0);-> output is false
+// console.log(null < 0);-> output is false
+
+// console.log(undefined>null);->false
+// console.log(undefined<null);->false
+
+// console.log("02" === 02) -> false
+// console.log(02 === 02)-> true
