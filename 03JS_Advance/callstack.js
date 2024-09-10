2da70823f726f62b216e61859bbcd3157ab0ef53
Aditya Kumar <131326798+Adityasinghvats@users.noreply.github.com>
2024-09-10 19:33:10 +0000

    long time no see

---
+++ 03JS_Advance/callstack.js
@@ -0,0 +1,5 @@
+// Javascript execution context
+// Global execution context -> this
+// Function execution context
+// Eval execution context
+// Phase 1 -> Memory phase
---
+++ 03JS_Advance/higherorderarrayloops.js
@@ -0,0 +1,11 @@
+const arr = [13,23,33,4,5];
+for(const num of arr){
+    // console.log(num)
+}
+
+
+// Forof loop
+const greet = "Hello Aditya!"
+for (const greetmsg of greet) {
+    console.log(`Each char is ${greetmsg}`);
+}
