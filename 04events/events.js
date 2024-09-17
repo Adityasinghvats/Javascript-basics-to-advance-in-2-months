49bed1c7fc7ad293296991e9b233bcd5c0254100
Aditya Kumar <131326798+Adityasinghvats@users.noreply.github.com>
2024-09-17 13:09:10 +0000

    Events

---
+++ 04events/events.js
@@ -0,0 +1,9 @@
+// All events in js run sequentially except some e.g async
+// Events invoke on some physical activity like mouse
+// Event listener = Broadcast receiver
+// Events to study:
+// type, timestamp, defaultPrevented, target, toElement, currentTarget, clientPosition
+// tilt, altkey, ctrlkey
+// Event bubbling -> on setting up multiple event listener , the smallest unit will 
+// alert first , then it will expand like bubble to outer element in html
+// Capturing mode -> outer to inner
